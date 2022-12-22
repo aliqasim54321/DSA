085e9ff6c4d5d35e6f3447f9368b68cbe81355c3
aliqasim54321 <aliqasimkhundmiri10@gmail.com>
2022-12-22 16:46:30 +0530

    basic.js

---
+++ basic.js
@@ -0,0 +1,50 @@
+//1. to print upto n natural number 
+//2. 
+
+// function avg(arr) {
+//    let sum = 0;
+//    for(let i =0;i<arr.length;i++){
+//     sum += arr[i];
+//    }
+//   return sum/arr.length;
+// }
+
+// //5
+
+// function greaterThanAvg(arr){
+//     awd
+// }
+
+//6 Find the largest element in an array
+// var arr = [3, 6, 2, 56, 32, 5, 89, 32];
+// var largest = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (largest < arr[i] ) {
+//         largest = arr[i];
+//     }
+// }
+// console.log(largest);
+
+//7 Find the smallest element in an array
+// var arr = [3, 6, 2];
+// var smallest = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (smallest > arr[i] ) {
+//         smallest = arr[i];
+//     }
+// }
+// console.log(smallest);
+
+//8 Find the given element in an array and return it's index if present else -1.
+var arr = [3,5,6,7];
+var num = arr[0];
+
+for(var i=0;i<= arr.length-1;i++){
+    if(num == i)
+    return i
+    else
+    return -1
+}
+
